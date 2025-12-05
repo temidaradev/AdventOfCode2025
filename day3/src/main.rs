acdab7ddd7b1842ba8d174387cad1bb6c1dd2762
temidaradev <temidaradev@proton.me>
2025-12-06 01:52:58 +0300

    day3 part2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,6 +1,7 @@
+use itertools::Itertools;
 use std::fs;
 
-fn get_max(input: &str) -> u64 {
+fn part1(input: &str) -> u64 {
     let mut total = 0;
 
     for line in input.lines() {
@@ -22,8 +23,43 @@ fn get_max(input: &str) -> u64 {
     total
 }
 
+pub fn part2(input: &str) -> String {
+    let result = input
+        .lines()
+        .map(|bank| {
+            let mut batteries: Vec<char> = vec![];
+
+            let mut current_index = 0;
+            for i in 0..11 {
+                let (index, first_max) = &bank[current_index..(bank.len() - 11 + i)]
+                    .chars()
+                    .enumerate()
+                    .max_set_by_key(|(_index, battery)| *battery)
+                    .first()
+                    .cloned()
+                    .unwrap();
+
+                batteries.push(*first_max);
+                current_index = current_index + index + 1;
+            }
+
+            let (_second_index, second_max) = &bank[(current_index)..]
+                .chars()
+                .enumerate()
+                .max_by_key(|(_index, battery)| *battery)
+                .unwrap();
+
+            batteries.push(*second_max);
+
+            batteries.iter().collect::<String>().parse::<u64>().unwrap()
+        })
+        .sum::<u64>();
+
+    result.to_string()
+}
+
 fn main() {
     let raw = fs::read_to_string("input.txt").unwrap();
-    let out = get_max(&raw);
+    let out = part2(&raw);
     println!("{out}");
 }
