d3aada4fff80450668ceccd78ea2bdbddc563076
temidaradev <temidaradev@proton.me>
2025-12-17 00:11:57 +0300

    all days folders for future

---
+++ day10/benches/benchmarks.rs
@@ -0,0 +1,17 @@
+use day10::{part1, part2};
+
+fn main() {
+    divan::main();
+}
+
+#[divan::bench]
+fn bench_part1() {
+    let input = include_str!("../input.txt");
+    divan::black_box(part1(input));
+}
+
+#[divan::bench]
+fn bench_part2() {
+    let input = include_str!("../input.txt");
+    divan::black_box(part2(input));
+}
---
+++ day10/src/main.rs
@@ -0,0 +1,8 @@
+use day10::{part1, part2};
+use std::fs;
+
+fn main() {
+    let raw = fs::read_to_string("input.txt").unwrap();
+    println!("Part 1: {}", part1(&raw));
+    println!("Part 2: {}", part2(&raw));
+}
